538221378355792ee0ae7eeb7aa063fbd357ce68
cwmli <calvinl9331@gmail.com>
2018-12-23 23:16:01 -0500

    Add WIP playlist endpoint

--- api/routes/spotify/data.js
+++ api/routes/spotify/data.js
@@ -2,8 +2,6 @@
 
 const request = require('request');
 
-const constants = require('./helper.js');
-
 module.exports = function(router) {
   
   router.get('/profile', function(req, res) {
@@ -23,6 +21,28 @@ module.exports = function(router) {
         res.send({
           success: false,
           message: 'error_retrieving_user'
+        });
+      }
+    });
+  });
+
+  router.get('/playlist', function(req, res) {
+    var auth = res.locals.auth;
+
+    request.get({
+      url: 'https://api.spotify.com/v1/me/playlists',
+      headers: { 'Authorization': 'Bearer ' + auth },
+      json: true
+    }, function(err, response, body) {
+      if (!err && response.statusCode == 200) {
+        res.send({
+          success: true
+          // some other payload here
+        })
+      } else {
+        res.send({
+          success: false,
+          message: 'error_retrieving_user_playlists'
         });
       }
     });
