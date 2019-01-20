a2888b3e22724b841043f620535d503a79d68e75
cwmli <calvinl9331@gmail.com>
2019-01-20 12:14:22 -0500

    Sequelize module

---
+++ README.md
@@ -0,0 +1,7 @@
+## Project Setup
+### Postgres
+```bash
+sudo -i -u postgres psql
+psql auralcordadmin -h 127.0.0.1 -d postgres
+CREATE DATABASE auralcord;
+```
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const config = require('./config/config');
+const config = require('./config/auralcord');
 
 const app = express();
 const port = process.env.PORT || 5000;
