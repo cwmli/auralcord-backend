4caffd8d11567ac5e897f19f60d9bf1ba20e8294
cwmli <calvinl9331@gmail.com>
2019-01-28 23:49:48 -0500

    Move to .env

--- config/auralcord.js
+++ config/auralcord.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const Config = {
-  SPOTIFY_CLIENT_ID: 'aa71a911355b47dcb387ba6f4abcd08d',
-  SPOTIFY_SECRET: '316fd25189aa4e038b7efd33c43a9f2f',
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_KEY,
+  SPOTIFY_SECRET: process.env.SPOTIFY_SECRET,
   SPOTIFY_REDIRECT_URI: 'http://localhost:5000/spotify/signin/callback',
   SPOTIFY_SCOPES: 'user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative',
   AURALCORD_CLIENT: 'http://localhost:8080'
